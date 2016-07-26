340cb1e89415f98eb223e4849feecd4b9ed1a733
tlantz77 <tlantz@gmail.com>
2016-07-26 16:09:00 -0500

    finish challenge

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,31 @@
 class FixedArray
 
+  attr_accessor :arr
+
+  def initialize(size)
+    @arr = Array.new(size)
+  end
+
+  def check_bounds(index)
+    raise StandardError, 'Out of Bounds' if index >= @arr.length
+  end
+
+  def set(index, element)
+    check_bounds(index)
+    @arr[index] = element
+  end
+
+  def get(index)
+    check_bounds(index)
+    @arr[index]
+  end
+
 end
+
+# fixed = FixedArray.new(5)
+# # p fixed.get(6)
+# #
+# # fixed.set(6, "hallo!")
+# # p fixed.get(6)
+#
+# fixed.get(6)
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,29 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+
+  let(:fixed) { FixedArray.new(5) }
+
+  it 'can instantiate a new array of a given size' do
+    expect(fixed).to be_a FixedArray
+    expect(fixed.arr.length).to eq(5)
+  end
+
+  it 'can retrieve the element at a given index' do
+    expect(fixed.get(2)).to eq(nil)
+  end
+
+  it 'will raise error if given index is outside the bounds of the array' do
+    expect { fixed.get(6) }.to raise_error(StandardError, 'Out of Bounds')
+  end
+
+  it 'can assign an element to a given index' do
+    fixed.set(3, "What up!")
+    expect(fixed.get(3)).to eq("What up!")
+  end
+
+  it 'will raise error if set element is outside the bounds of the array' do
+    expect { fixed.set(6, "Yahoo!") }.to raise_error(StandardError, 'Out of Bounds')
+  end
+
 end
